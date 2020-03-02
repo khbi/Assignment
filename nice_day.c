853463d00b32252c85270636f5fa7419c2efa11f
khbi <gksqlrla09@gmail.com>
2020-03-03 01:00:26 +0900

    nice_day.c

---
+++ nice_day.c
@@ -0,0 +1,30 @@
+/* In file nice_day.c */
+/* Have a nice day! */
+
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXSTRING	100
+
+int main(void){
+	char	c, name[MAXSTRING];
+	int	i, sum = 0;
+
+	printf("\nHi !  What is your name?   ");
+	for (i = 0; (c = getchar()) != '\n'; ++i){
+		name[i] = c;
+		if (isalpha(c))
+			sum += c;
+	}
+	name[i] = '\0';
+	printf("\n%s%s%s\n%s",
+			"Nice to meet you ", name, ".",
+			"Your namespelled backwards is ");
+	for (--i; i >= 0; --i)
+		putchar(name[i]);
+	printf("\n%s%d%s\n\n%s\n",
+			"and the letters in your name sum to ", sum, ".",
+			"Have a nice day!");
+	return 0;
+}
+
