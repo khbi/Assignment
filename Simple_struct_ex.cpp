fc7ab746c802a1d117be5f37e5f8c34f0bb17473
khbi <55778090+khbi@users.noreply.github.com>
2020-04-03 13:53:40 +0900

    Create Simple_struct_ex.cpp
    This is Simple struct example in C++

---
+++ Simple_struct_ex.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+	int xpos;
+	int ypos;
+
+    void MovePos(int x, int y){
+        xpos += x;
+        ypos += y;
+        return;
+    }// Coordinate movement of points
+    void AddPoint(const Point &pos) {
+        xpos += pos.xpos;
+        ypos += pos.ypos;
+        return; 
+
+        
+    }// Coordinate increase of points
+    void ShowPosition(){
+        cout << "[" << xpos << "," << ypos << "]" << endl;
+
+    }// Output current x, y coordinate information
+};
+
+
+
+int main(void) {
+    Point pos1 = { 12, 4 };
+    Point pos2 = { 20, 30 };
+
+    pos1.MovePos(-7, 10);
+    pos1.ShowPosition();		//Print [5, 14]
+
+    pos1.AddPoint(pos2);
+    pos1.ShowPosition();		//Print [25, 44]
+
+    return 0;
+}
